271842f0953852488ef5504d2b0f602c6bd93827
Favor Vincent <vfavorm77@gmail.com>
2024-03-15 02:21:15 +0300

    JS intro to functions lab complete

---
+++ index.js
@@ -0,0 +1,29 @@
+function shout(string){
+return string.toUpperCase();
+}
+
+function whisper(string){
+    return string.toLowerCase();
+}
+
+function logShout(string){
+console.log(string.toUpperCase());
+}
+ 
+function logWhisper(string){
+console.log(string.toLowerCase());
+}
+
+  function sayHiToHeadphonedRoommate(string) {
+    if (string === 'hello')
+     return"I can\'t hear you!"
+        else if (string === 'HELLO')
+      return "YES INDEED!"
+       else (string === "Let's have dinner together!")
+       return "I would love to!"
+  }
+
+  (sayHiToHeadphonedRoommate('HELLO')).toEqual("YES INDEED!");
+  (sayHiToHeadphonedRoommate('hello')).toEqual("I can't hear you!");
+  (sayHiToHeadphonedRoommate("Let's have dinner together!")).toEqual("I would love to!");
+
